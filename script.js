fb7b940d73c9011f703591542f3161f4550a4a3a
Lulibo <leelulibo>
2024-08-20 18:29:29 +0200

    danko for the day

--- script.js
+++ script.js
@@ -11,3 +11,35 @@ var swiper = new Swiper(".home-slider", {
         prevEl: ".swiper-button-prev", // Corrected property name
     }
 });
+
+var swiper = new Swiper(".room-slider", {
+    spaceBetween: 20,
+    grabCursor: true,
+    loop: true,
+    centeredSlides: true,
+    autoplay: {
+        delay: 7500,
+        disableOnInteraction: false,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    breakpoints: {
+        0: {
+            slidesPerView: 1, // Fixed typo: slidePerView -> slidesPerView
+        },
+        768: {
+            slidesPerView: 2, // Fixed typo: slidePerView -> slidesPerView
+        },
+        991: {
+            slidesPerView: 3, // Fixed typo: slidePerView -> slidesPerView
+        },
+    }
+});
+
+
