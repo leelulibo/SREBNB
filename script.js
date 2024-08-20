a1d404c2434dad51290c88f6aceccda0b394c4ff
Lulibo <leelulibo>
2024-08-20 16:37:57 +0200

    website first code

---
+++ script.js
@@ -0,0 +1,13 @@
+var swiper = new Swiper(".home-slider", {
+    grabCursor: true,
+    loop: true,
+    centeredSlides: true,
+    autoplay: {
+        delay: 7500,
+        disableOnInteraction: false,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next", // Corrected property name
+        prevEl: ".swiper-button-prev", // Corrected property name
+    }
+});
